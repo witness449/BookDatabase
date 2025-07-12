3d81898149ba2ff204dcc357afd9190dac97ccfd
witness449 <kesha-84@yandex.ru>
2025-07-12 18:48:31 +0000

    Подготовка гетерогенного сравнения

--- include/heterogeneous_lookup.hpp
+++ include/heterogeneous_lookup.hpp
@@ -5,10 +5,29 @@
 
 namespace bookdb {
 
-struct TransparentStringLess {};
+struct TransparentStringLess {
+    using is_transparent=void;
+    template <typename T>
+    bool operator()(const T& a, std::string_view b) const {
+        return std::less<>()(a, b); // Используем стандартную функцию сравнения, адаптированную к типам
+    }
+};
 
-struct TransparentStringEqual {};
+struct TransparentStringEqual {
+    using is_transparent=void;
+    template <typename T>
+    friend auto operator==(const T & lhs, std::string_view rhs){return lhs == rhs; };
+    template <typename T>
+    friend auto operator==(std::string_view lhs, const T& rhs){return rhs == lhs; };
 
-struct TransparentStringHash {};
+};
+
+struct TransparentStringHash {
+    using is_transparent=void;
+    /*template <typename T>
+    friend auto operator==(const T & lhs, std::string_view rhs){return lhs == std::hash<std::string_view>(rhs){}; };
+    template <typename T>
+    friend auto operator==(std::string_view lhs, const T& rhs){return rhs == lhs; };*/
+};
 
 }  // namespace bookdb
